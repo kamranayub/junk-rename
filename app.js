fc49927acae4195c74b9bc999bf8b60846cf1228
Kamran Ayub <kamran.ayub@gmail.com>
2019-06-20 19:43:37 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+function app() {
+  const hello = "world";
+  return `
+    <html>
+    <head>
+    <title>Hello World</title>
+    </head>
+    <body>${hello}</body>
+    </html>
+  `
+}
+
+export default app;
